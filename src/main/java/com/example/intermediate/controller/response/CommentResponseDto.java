738ef9b71e850be0e1ceecafcf33d5f4e497861b
kidonge <ehdgus3519@naver.com>
2022-10-20 16:51:57 +0900

    CommentResponseDto getMember()에 getNickname 추가

--- src/main/java/com/example/intermediate/controller/response/CommentResponseDto.java
+++ src/main/java/com/example/intermediate/controller/response/CommentResponseDto.java
@@ -29,7 +29,7 @@ public class CommentResponseDto {
   public static List<CommentResponseDto> toDtoList(List<Comment> comments) {
     NestedConvertHelper helper = NestedConvertHelper.newInstance(
             comments,
-            c -> new CommentResponseDto(c.getId(), c.isRemoved() ? null : c.getContent(), c.isRemoved() ? null : String.valueOf(c.getMember()), c.getCreatedAt(), c.getModifiedAt(),new ArrayList<>()),
+            c -> new CommentResponseDto(c.getId(), c.isRemoved() ? null : c.getContent(), c.isRemoved() ? null : String.valueOf(c.getMember().getNickname()), c.getCreatedAt(), c.getModifiedAt(),new ArrayList<>()),
             c -> c.getParent(),
             c -> c.getId(),
             d -> d.getChildren());
